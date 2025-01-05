b8321a1e660b0ab4f92712c102c058db8c2b426e
Tiberio <tiberio.brinzea@gmail.com>
2025-01-05 19:05:16 +0200

    Radzen integration

--- MeteoApp/MeteoApp/Program.cs
+++ MeteoApp/MeteoApp/Program.cs
@@ -1,27 +1,29 @@
 using MeteoApp.Components;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
+    .AddInteractiveServerComponents(); // Add server-side interactive component support
+builder.Services.AddSingleton<ThemeService>();
+builder.Services.AddRadzenComponents();
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHsts(); // Add HSTS for production
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+// Map the Razor components and set the render mode to InteractiveServerRenderMode
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode();  // Add this to enable Interactive server render mode
 
 app.Run();
